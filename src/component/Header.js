83dd02ef71ae250c3c384fb42146899559badbd8
Prsh1234 <shakya.pratyush789@gmail.com>
2025-08-18 13:50:03 +0545

    added friend profile management

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,20 +1,96 @@
 import { useNavigate } from "react-router";
+import { useState } from "react";
 import "../css/Header.css";
 import { doLogout } from "../services/auth";
-const Header = () => {
+import axios from "axios";
+import CONFIG from "../config";
+
+const Header = ({ currentUserId }) => {
     const navigate = useNavigate();
+    const [query, setQuery] = useState("");
+    const [results, setResults] = useState([]);
+    const userId = localStorage.getItem("userId");
     const handleLogout = () => {
         doLogout(navigate);
-    }
+    };
+
+    const handleSearch = async (e) => {
+        const value = e.target.value;
+        setQuery(value);
+
+        if (value.trim().length > 0) {
+            try {
+                const res = await axios.get(
+                    `${CONFIG.API_URL}/user/search`,
+                    { params: { username: value } }
+                );
+                setResults(res.data);
+            } catch (err) {
+                console.error("Search error:", err);
+            }
+        } else {
+            setResults([]);
+        }
+    };
+
+    const handleUserClick = (id) => {
+        // Convert both to number
+        const clickedId = Number(id);
+        const currentId = Number(userId);
+
+        if (clickedId === currentId) {
+            navigate(`/profile/info`);
+            console.log("profile");
+        } else {
+            navigate(`/friend/info/${clickedId}`);
+            console.log("friend");
+        }
+
+        setQuery("");
+        setResults([]);
+    };
+
+
     return (
         <div className="row Header">
             <div className="header-nav-links">
-                <p>Header</p>
+                <input
+                    type="text"
+                    placeholder="Search ..."
+                    value={query}
+                    onChange={handleSearch}
+                    className="search-input"
+                />
+                {results.length > 0 && (
+                    <ul className="search-results">
+                        {results.map((user) => (
+                            <li
+                                key={user.id}
+                                onClick={() => handleUserClick(user.id)}
+                                className="search-result-item"
+                            >
+                                    <img
+                                        src={
+                                            user.profilePic
+                                                ? `data:image/jpeg;base64,${user.profilePic}`
+                                                : "/assets/profile.jpg"
+                                        }
+                                        alt="profile"
+                                        className="search-avatar"
+                                    />
+
+                                <span>{user.userName}</span>
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
+
             <div className="header-homelink">
                 <button onClick={handleLogout} className="logout">Logout</button>
             </div>
         </div>
     );
-}
+};
+
 export default Header;
--- src/component/friend/FriendHeader.js
+++ src/component/friend/FriendHeader.js
@@ -2,57 +2,128 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import "../../css/ProfileHeader.css";
 import { getUserData } from "../../services/user";
+import { sendFriendRequest, unfriend, GetFriendRequests, acceptFriendRequest, rejectFriendRequest } from "../../services/friend";
+import axios from "axios";
+import CONFIG from "../../config";
 
 const FriendHeader = () => {
-    const [coverPreview, setCoverPreview] = useState("/assets/cover.jpg");
-    const [profilePreview, setProfilePreview] = useState("/assets/profile.jpg");
-    const [username, setUsername] = useState("");
-    const { friendId } = useParams();
+  const [coverPreview, setCoverPreview] = useState("/assets/cover.jpg");
+  const [profilePreview, setProfilePreview] = useState("/assets/profile.jpg");
+  const [username, setUsername] = useState("");
+  const [friendStatus, setFriendStatus] = useState(""); 
 
+  const { friendId } = useParams();
+  const [requestId, setRequestId] = useState({});
+  const userId = Number(localStorage.getItem("userId"));
 
-    const fetchUser = async (friendId) => {
-        try {
-            const res = await getUserData(friendId);
-            const user = res.data;
+  const fetchUser = async (friendId) => {
+    try {
+      const res = await getUserData(friendId);
+      const user = res.data;
 
-            setCoverPreview(
-                user.coverPhoto
-                    ? `data:image/jpeg;base64,${user.coverPhoto}`
-                    : "/assets/cover.jpg"
-            );
-            setProfilePreview(
-                user.profilePic
-                    ? `data:image/jpeg;base64,${user.profilePic}`
-                    : "/assets/profile.jpg"
-            );
-            setUsername(user.userName || `${user.firstName} ${user.lastName}`);
-        } catch (error) {
-            console.error("Error fetching user:", error);
-        }
-    };
+      setCoverPreview(
+        user.coverPhoto
+          ? `data:image/jpeg;base64,${user.coverPhoto}`
+          : "/assets/cover.jpg"
+      );
+      setProfilePreview(
+        user.profilePic
+          ? `data:image/jpeg;base64,${user.profilePic}`
+          : "/assets/profile.jpg"
+      );
+      setUsername(user.userName || `${user.firstName} ${user.lastName}`);
+    } catch (error) {
+      console.error("Error fetching user:", error);
+    }
+  };
 
+  const fetchFriendStatus = async () => {
+    try {
+      const res = await axios.get(`${CONFIG.API_URL}/friend/status`, {
+        params: { currentUserId: userId, otherUserId: Number(friendId) }
+      });
+      setRequestId(res.data.requestId);
+      setFriendStatus(res.data.status);
+    } catch (err) {
+      console.error("Error fetching friend status:", err);
+    }
+  };
+  
 
-    useEffect(() => {
-        fetchUser(friendId);
-      }, [friendId]);
+  useEffect(() => {
+    fetchUser(friendId);
+    fetchFriendStatus();
+  }, [friendId]);
 
+  // Action handlers
+  const handleSendRequest = async () => {
+    const res = await sendFriendRequest(Number(friendId));
+    if (res.success) setFriendStatus("request-sent");
+    fetchFriendStatus();
 
+  };
 
-    return (
-        <div className="profile-header">
-            <div className="cover-photo-wrapper">
-                <img src={coverPreview} alt="Cover" className="cover-image" />
-            </div>
+  const handleUnfriend = async () => {
+    const res = await unfriend(Number(friendId));
+    if (res.success) setFriendStatus("not-friend");
+    fetchFriendStatus();
 
-            {/* Profile Info */}
-            <div className="profile-info">
-                <div className="profile-pic-wrapper">
-                    <img className="profile-image" src={profilePreview} alt="Profile" />
-                </div>
-                <h2 className="username">{username}</h2>
-            </div>
+  };
+
+  const handleAcceptRequest = async () => {
+    // Find the request id from API
+    const res = await acceptFriendRequest(requestId);
+
+    if (res.success) {
+      setFriendStatus("friend");
+      fetchFriendStatus();
+
+    }
+  };
+
+  const handleCancelRequest = async () => {
+    // Here we can reject the request sent by the user
+    const res = await rejectFriendRequest(requestId);
+
+    if (res.success) {
+      setFriendStatus("not-friend");
+      fetchFriendStatus();
+
+    }
+  };
+
+  return (
+    <div className="profile-header">
+      <div className="cover-photo-wrapper">
+        <img src={coverPreview} alt="Cover" className="cover-image" />
+      </div>
+
+      <div className="profile-info">
+        <div className="profile-pic-wrapper">
+          <img className="profile-image" src={profilePreview} alt="Profile" />
         </div>
-    );
+        <h2 className="username">{username}</h2>
+
+        {/* Friend Action Button */}
+        {Number(friendId) !== userId && (
+          <div className="friend-action">
+            {friendStatus === "friend" && (
+              <button onClick={handleUnfriend}>Unfriend</button>
+            )}
+            {friendStatus === "not-friend" && (
+              <button onClick={handleSendRequest}>Add Friend</button>
+            )}
+            {friendStatus === "request-sent" && (
+              <button onClick={handleCancelRequest}>Cancel Request</button>
+            )}
+            {friendStatus === "request-received" && (
+              <button onClick={handleAcceptRequest}>Accept Request</button>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FriendHeader;
